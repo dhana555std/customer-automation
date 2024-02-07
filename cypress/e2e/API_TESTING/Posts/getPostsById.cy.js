60b583b62b471d92ac67a535dee44ec6bb6eb1b0
dhana555std <dhanapathi.marepalli@gmail.com>
2024-02-07 11:43:41 +0000

    GEN-41 Add all files for Cypress which are generated by Cypress Swagger Autocodegen.

---
+++ cypress/e2e/API_TESTING/Posts/getPostsById.cy.js
@@ -0,0 +1,38 @@
+const apiBaseURL = Cypress.env('CYPRESS_BASE_URL');
+const requestInfo = JSON.parse(
+    JSON.stringify({ url: '/posts/{postId}', method: 'GET' })
+);
+requestInfo.url = apiBaseURL + requestInfo.url;
+
+describe('fetches the data of a perticular post.', () => {
+    it('200 GET application/json  successful operation', () => {
+        cy.fixture('200_application_json__getPostsById').then(
+            (fixtureResponse) => {
+                requestInfo.body = fixtureResponse.payload
+                    ? fixtureResponse.payload
+                    : '';
+                requestInfo.headers = fixtureResponse.headers
+                    ? fixtureResponse.headers
+                    : '';
+                let pathParams = fixtureResponse.pathParam
+                    ? fixtureResponse.pathParam
+                    : '';
+                for (const key in pathParams) {
+                    if (pathParams.hasOwnProperty(key)) {
+                        const placeholder = '{' + key + '}';
+                        requestInfo.url = requestInfo.url.replace(
+                            new RegExp(placeholder, 'g'),
+                            pathParams[key]
+                        );
+                    }
+                }
+
+                cy.request(requestInfo).then((response) => {
+                    expect(response.status).to.eq(
+                        parseInt(fixtureResponse.responseStatusCode)
+                    );
+                });
+            }
+        );
+    });
+});
